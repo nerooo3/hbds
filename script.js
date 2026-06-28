d7fad0a392ff1423234a8217474121a4399301ed
nerooo3 <shadfarhad251@gmail.com>
2026-06-28 21:15:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,45 +3,13 @@ const message = document.getElementById("message");
 const music = document.getElementById("music");
 
 button.addEventListener("click", () => {
-
     message.style.display = "block";
 
     if (music) {
-        music.play();
+        music.play().catch(() => {
+            console.log("Music couldn't start.");
+        });
     }
 
     button.innerHTML = "💖 I Love You Soma 💖";
 });
-
-function createHeart() {
-
-    const heart = document.createElement("div");
-
-    heart.innerHTML = "💖";
-
-    heart.style.position = "fixed";
-    heart.style.left = Math.random() * 100 + "vw";
-    heart.style.top = "-30px";
-    heart.style.fontSize = (15 + Math.random() * 25) + "px";
-    heart.style.pointerEvents = "none";
-    heart.style.zIndex = "9999";
-
-    document.body.appendChild(heart);
-
-    let y = -30;
-
-    const animation = setInterval(() => {
-
-        y += 3;
-
-        heart.style.top = y + "px";
-
-        if (y > window.innerHeight) {
-            clearInterval(animation);
-            heart.remove();
-        }
-
-    }, 20);
-}
-
-setInterval(createHeart, 400);
